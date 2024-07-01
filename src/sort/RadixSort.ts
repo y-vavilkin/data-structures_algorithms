7806bbc3ce4ad89241ab96fb58a797624c0c1ba9
Yaroslav Vavilkin <y.vavilkin@tg.dunice.net>
2024-07-01 16:30:54 +0300

    feat: algorithm radix sort

---
+++ src/sort/RadixSort.ts
@@ -0,0 +1,26 @@
+// Устойчивая
+
+// Time Complexity                          Space Complexity
+// Best         Average        Worst        Worst
+// O(nk)	      O(nk)	         O(nk)        O(n+k)
+function radixSort(arr: number[]): number[] {
+  const maxNumber = Math.max(...arr);
+  const maxDigits = maxNumber.toString().length;
+
+  const base = 10;
+
+  for (let i = 0; i < maxDigits; i++) {
+      const bins: number[][] = Array.from({ length: base }, () => []);
+
+      arr.forEach((x) => {
+          const digit = Math.floor(x / Math.pow(base, i)) % base;
+          bins[digit].push(x);
+      });
+
+      arr = ([] as number[]).concat(...bins);
+
+  }
+  return arr;
+}
+
+export default radixSort;
--- src/sort/index.ts
+++ src/sort/index.ts
@@ -1,3 +1,3 @@
 export { default as mergeSort } from './MergeSort';
 export { default as quickSort } from './QuickSort';
-export { default as heapSort } from './HeapSort';
+export { default as radixSort } from './RadixSort';
