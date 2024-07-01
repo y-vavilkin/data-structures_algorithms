3467620447f930badfed97f532bb498abddf269b
Yaroslav Vavilkin <y.vavilkin@tg.dunice.net>
2024-07-01 17:07:28 +0300

    style: fix tabs in Queue.ts

--- src/structures/Queue(FIFO).ts
+++ src/structures/Queue(FIFO).ts
@@ -3,8 +3,8 @@ class NodeItem<T> {
   next: NodeItem<T> | null;
 
   constructor(value: T) {
-      this.value = value;
-      this.next = null;
+    this.value = value;
+    this.next = null;
   }
 }
 
@@ -14,37 +14,37 @@ class Queue<T> {
   private tail: NodeItem<T> | null;
 
   constructor() {
-      this.length = 0;
-      this.head = null;
-      this.tail = null;
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
   }
 
   push(value: T): number {
-      const node = new NodeItem(value);
+    const node = new NodeItem(value);
 
-      if (this.head === null || this.tail === null) {
-          this.head = node;
-          this.tail = node;
-      } else {
-          this.tail.next = node;
-          this.tail = node;
-      }
-      return ++this.length;
+    if (this.head === null || this.tail === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    return ++this.length;
   }
 
   pop(): NodeItem<T> | null {
-      const node = this.head;
-      this.head = node?.next ?? null;
-      this.length--;
-      return node;
+    const node = this.head;
+    this.head = node?.next ?? null;
+    this.length--;
+    return node;
   }
 
   print(): void {
-      let current = this.head;
-      while (current !== null) {
-          console.log(current.value);
-          current = current.next;
-      }
+    let current = this.head;
+    while (current !== null) {
+      console.log(current.value);
+      current = current.next;
+    }
   }
 }
 
