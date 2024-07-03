e46d12d93a8e4a9f60527be462d8f51353211289
Yaroslav Vavilkin <y.vavilkin@tg.dunice.net>
2024-07-03 09:31:25 +0300

    refactor: update algorithm MergeSort

--- src/sort/MergeSort.ts
+++ src/sort/MergeSort.ts
@@ -3,32 +3,39 @@
 // Time Complexity                              Space Complexity
 // Best         Average         Worst           Worst
 // O(n*log(n))  O(n*log(n))     O(n*log(n))     O(n)
-const mergeSort = <T>(arr: T[]): T[] => {
-  if (arr.length <= 2) {
-    if (arr?.[0] > arr?.[1]) return arr.reverse();
-    return arr;
+const mergeSort = (arr: number[]): number[] => {
+  if (arr.length <= 1) {
+      return arr;
   }
 
-  const left = mergeSort(arr.slice(0, arr.length/2));
-  const right = mergeSort(arr.slice(arr.length/2, arr.length));
-  return [...merge(left, right)];
+  const mid = Math.floor(arr.length / 2);
+
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
 }
 
-const merge = <T>(left: T[], right: T[]): T[] => {
-  const result = [];
-  let [leftIndex, rightIndex] = [0, 0];
+const merge = (left: number[], right: number[]): number[] => {
+  const result: number[] = [];
 
-  while (leftIndex !== left.length && rightIndex !== right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      result.push(left[leftIndex]);
-      leftIndex++;
-    } else {
-      result.push(right[rightIndex]);
-      rightIndex++;
-    }
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+          result.push(left[leftIndex]);
+          leftIndex++;
+      } else {
+          result.push(right[rightIndex]);
+          rightIndex++;
+      }
   }
 
-  return [...result, ...left.slice(leftIndex), ...right.slice(rightIndex)];
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 export default mergeSort;
